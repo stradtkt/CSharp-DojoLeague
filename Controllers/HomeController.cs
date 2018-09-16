35ba54176992d96a94dff9f63d3143649f992b20
Kevin Stradtman <stradtkt@gmail.com>
2018-09-16 01:38:34 -0400

    Added Ninjas page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,27 +5,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DojoLeague.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DojoLeague.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private DojoContext _dContext;
+        public HomeController(DojoContext context)
         {
+            _dContext = context;
+        }
+        [HttpGet("")]
+        public IActionResult Ninjas()
+        {
+            List<Dojo> Dojos = _dContext.dojos.ToList();
+            List<Ninja> Ninjas = _dContext.ninjas.Include(d => d.Dojos).ToList();
+            ViewBag.dojos = Dojos;
+            ViewBag.ninjas = Ninjas;
             return View();
         }
-
-        public IActionResult About()
+        [HttpGet("Dojos")]
+        public IActionResult Dojos()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
--- Startup.cs
+++ Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DojoLeague.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
