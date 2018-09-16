408466bd92fb8f39de0cbaf46607ce515b02d788
Kevin Stradtman <stradtkt@gmail.com>
2018-09-16 01:50:17 -0400

    Added more to the homecontroller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,47 @@ namespace DojoLeague.Controllers
         [HttpGet("Dojos")]
         public IActionResult Dojos()
         {
+            List<Dojo> Dojos = _dContext.dojos.ToList();
+            List<Ninja> Ninjas = _dContext.ninjas.Include(d => d.Dojos).ToList();
+            ViewBag.dojos = Dojos;
+            ViewBag.ninjas = Ninjas;
+            return View();
+        }
+
+        [HttpPost("AddNinja")]
+        public IActionResult AddNinja(Ninja nin)
+        {
+            Ninja ninja = new Ninja
+            {
+                name = nin.name,
+                level = nin.level,
+                description = nin.description,
+                dojo_id = nin.dojo_id
+            };
+            _dContext.ninjas.Add(ninja);
+            _dContext.SaveChanges();
+            return RedirectToAction("Ninjas");
+        }
+
+        [HttpPost("AddDojo")]
+        public IActionResult AddDojo(Dojo doj)
+        {
+            Dojo dojo = new Dojo
+            {
+                name = doj.name,
+                location = doj.location,
+                description = doj.description,
+            };
+            _dContext.dojos.Add(dojo);
+            _dContext.SaveChanges();
+            return RedirectToAction("Dojos");
+        }
+
+        [HttpGet("Ninja/{ninja_id}")]
+        public IActionResult Ninja(int ninja_id)
+        {
+            Ninja ninja = _dContext.ninjas.Include(d => d.Dojos).Where(n => n.ninja_id == ninja_id).SingleOrDefault();
+            ViewBag.ninja = ninja;
             return View();
         }
 
